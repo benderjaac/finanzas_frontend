88a2b5551382f60ef8c38cd118d3b5000eadc68a
Angel Arango <arangocruzjoseangel@gmail.com>
2025-04-02 14:29:54 -0600

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { Toast } from 'primeng/toast';
+import { ButtonModule } from 'primeng/button';
+import { Ripple } from 'primeng/ripple';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Toast, ButtonModule, RouterOutlet, Ripple ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [MessageService]
 })
 export class AppComponent {
-  title = 'primeng';
+  title = 'Ejemplos';
+  constructor(
+    private messageService: MessageService
+  ){
+  }
+
+  options = signal<string[]>(['Mexico','Jalisco','Oaxaca']);
+  selectedOption=signal<number>(-1);
+
+  selectOption(index:number):void{
+    this.selectedOption.set(index);
+  }
+
+  show() {
+    this.messageService.add(
+      { severity: 'info', summary: 'Info', detail: 'Message Content', life: 3000 }
+    );
+  }
+
+
 }
