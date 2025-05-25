45e59071c7c1bd962098c2ae4e32e135b60bb526
Angel Arango <arangocruzjoseangel@gmail.com>
2025-05-24 23:11:02 -0600

    agregar routerLink a menus

--- src/app/layout/menu/menu.component.ts
+++ src/app/layout/menu/menu.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { AuthService, PermisoDTO } from 'app/auth/auth.service';
 import { Ripple } from 'primeng/ripple';
 import { StyleClass } from 'primeng/styleclass';
 
 @Component({
   selector: 'app-menu',
-  imports: [Ripple, StyleClass, CommonModule],
+  imports: [Ripple, StyleClass, CommonModule, RouterModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
