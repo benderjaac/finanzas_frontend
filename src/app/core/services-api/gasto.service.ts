2039f8d1ad2d4e27085e7558b6d2a2e381cb23c3
Angel Arango <arangocruzjoseangel@gmail.com>
2025-05-30 11:27:53 -0600

    copiar funcionamiento de tabla a gastos

--- src/app/core/services-api/gasto.service.ts
+++ src/app/core/services-api/gasto.service.ts
@@ -4,6 +4,7 @@ import { environment } from 'environments/environment';
 import { Gasto } from '../models/gasto.model';
 import { Observable } from 'rxjs';
 import { ResponseApiType } from '../models/response-api.model';
+import { ApiQuery } from '../models/query.model';
 
 @Injectable({
   providedIn: 'root'
@@ -20,5 +21,9 @@ export class GastoService {
 
   getGastos():Observable<ResponseApiType<Gasto>>{
     return this._httpClient.get<ResponseApiType<Gasto>>(this.urlApi+'/api/gastos');
+  }
+
+  getDataGastos(query: ApiQuery):Observable<any>{
+    return this._httpClient.post<any>(this.urlApi+'/api/gastos/data', query);
   }
 }
--- src/app/modules/finanzas/gasto/gasto-list/gasto-list.component.ts
+++ src/app/modules/finanzas/gasto/gasto-list/gasto-list.component.ts
@@ -4,8 +4,9 @@ import { Gasto } from 'app/core/models/gasto.model';
 import { ResponseApiType } from 'app/core/models/response-api.model';
 import { GastoService } from 'app/core/services-api/gasto.service';
 import { Subject, takeUntil } from 'rxjs';
-import { TableModule } from 'primeng/table';
+import { TableLazyLoadEvent, TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
+import { ApiQuery } from 'app/core/models/query.model';
 
 @Component({
   selector: 'app-gasto-list',
@@ -15,31 +16,66 @@ import { ButtonModule } from 'primeng/button';
 export class GastoListComponent {
 
   gastos : Gasto[] = [];
+  totalRecords = 0;
+  
+  rowsPerPageOptions: number[] = [];
+  rowsDefault = 0;
+
+  loading = false;
 
   destroy$ = new Subject<void>();
 
   constructor(
     private _gastoService: GastoService,
   ){
-
+    this.rowsPerPageOptions = [10, 20, 50, 100]
+    this.rowsDefault = this.rowsPerPageOptions[0];
   }
 
   ngOnInit():void{
-    this._gastoService.getGastos()
-      .pipe(takeUntil(this.destroy$))
-      .subscribe({
-        next: (response: ResponseApiType<Gasto>) =>{
-          this.gastos = response.data;
-        },
-        error: () =>{
-
-        }
-      });
+    
   }
 
   ngOnDestroy():void{
     this.destroy$.next();
     this.destroy$.complete();
+  }
+
+  getgastosData(event: TableLazyLoadEvent):void{
+      
+    this.loading = true;
+
+    const page = (event.first ?? 0) / (event.rows ?? 10) + 1;
+    const perPage = event.rows ?? 10;
+
+    const query: ApiQuery = {
+      filters: [], // Más adelante puedes llenar con filtros
+      sorter: typeof event.sortField === 'string'
+      ? [{
+          field: event.sortField,
+          order: event.sortOrder === 1 ? 'ASC' : 'DESC',
+        }]
+      : [],
+      pagination: {
+        perPage,
+        currentPage: page,
+      },
+    };
+
+    this._gastoService.getDataGastos(query)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (res)=>{
+          this.gastos = res.result.data;
+          this.totalRecords = res.result.pagination.totalItems;
+          this.loading = false;          
+        },
+        error: (error)=> {
+          this.gastos=[];
+          this.totalRecords = 0;
+          this.loading = false;
+        }
+      });
   }
 
   addGasto():void{
