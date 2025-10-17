c71378b843cd5b0b04256bffd4ccb1a2039a7dfe
Angel <arangocruzjoseangel@gmail.com>
2025-10-17 01:02:39 -0600

    feat(resumen):grafica de gastos acumulados por mes

--- src/app/core/services-api/estadisticas.service.ts
+++ src/app/core/services-api/estadisticas.service.ts
@@ -4,6 +4,7 @@ import { environment } from 'environments/environment';
 import { Observable } from 'rxjs';
 import { ResponseApiCat, ResponseApiSimple } from '../models/response-api.model';
 import { TotalMeses } from '../models/estadisticas.model';
+import { Movimiento } from '../models/movimiento.model';
 
 @Injectable({
   providedIn: 'root'
@@ -19,6 +20,10 @@ export class EstadisticasService {
 
   getTotalMeses():Observable<ResponseApiCat<TotalMeses>>{
     return this._httpClient.get<ResponseApiCat<TotalMeses>>(this.urlApi+'/api/estadisticas/totalesmes');
+  }
+
+  getGastosAcumulados():Observable<ResponseApiCat<Movimiento>>{
+    return this._httpClient.get<ResponseApiCat<Movimiento>>(this.urlApi+'/api/estadisticas/gastos3meses');
   }
   
 }
--- src/app/modules/finanzas/resumen/resumen.component.ts
+++ src/app/modules/finanzas/resumen/resumen.component.ts
@@ -7,6 +7,7 @@ import {CurrencyPipe, DatePipe, isPlatformBrowser} from '@angular/common';
 import { EstadisticasService } from 'app/core/services-api/estadisticas.service';
 import { TotalMeses } from 'app/core/models/estadisticas.model';
 import { DateFormatsService } from '../servicios/DateFormats.service';
+import { Movimiento } from 'app/core/models/movimiento.model';
 
 @Component({
   selector: 'app-resumen',
@@ -21,6 +22,10 @@ export class ResumenComponent {
   DataTotalMesesAPI:TotalMeses[]=[];
   DataTotalMeses:any=undefined;
   OptionsTotalMeses:any=undefined;
+
+  DataGastosAcumuladosAPI:Movimiento[]=[];
+  DataGastosAcumulados:any=undefined;
+  OptionsGastosAcumulados:any=undefined;
 
   platformId = inject(PLATFORM_ID);
 
@@ -37,16 +42,28 @@ export class ResumenComponent {
 
   ngOnInit(): void {
     this.cargarDataBalanceUsuario();
+
     this._estadisticasService.getTotalMeses().pipe(takeUntil(this.destroy$))
       .subscribe({
         next: (response) => {
           this.DataTotalMesesAPI = response.result.data;
-          this.initChart();
+          this.initChartTotalesMes();
         },
         error: (error) => {
           console.error('Error al cargar los datos del total por meses:', error);
         }
       });    
+
+      this._estadisticasService.getGastosAcumulados().pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (response) => {
+          this.DataGastosAcumuladosAPI = response.result.data;
+          this.initChartGastosAcumulados();
+        },
+        error: (error) => {
+          console.error('Error al cargar los datos del total por meses:', error);
+        }
+      });  
   }
 
   cargarDataBalanceUsuario(): void {
@@ -62,7 +79,7 @@ export class ResumenComponent {
         }
       });
   }
-  initChart() {
+  initChartTotalesMes() {
     if (isPlatformBrowser(this.platformId)) {
       const documentStyle = getComputedStyle(document.documentElement);
       const textColor = documentStyle.getPropertyValue('--p-text-color');
@@ -130,6 +147,118 @@ export class ResumenComponent {
       };
       this.cd.markForCheck()
     }
+  }
+
+  initChartGastosAcumulados() {
+    if (isPlatformBrowser(this.platformId)) {
+      const documentStyle = getComputedStyle(document.documentElement);
+      const textColor = documentStyle.getPropertyValue('--p-text-color');
+      const textColorSecondary = documentStyle.getPropertyValue('--p-text-muted-color');
+      const surfaceBorder = documentStyle.getPropertyValue('--p-content-border-color');
+
+      const labels = Array.from({ length: 31 }, (_, i) => (i + 1).toString());
+
+      const mesesUnicos = Array.from(
+        new Set(
+          this.DataGastosAcumuladosAPI.map(d => d.fecha.slice(0, 7)) // formato 'YYYY-MM'
+        )
+      ).sort();
+
+      const [preanteriorMes, anteriorMes, actualMes] = mesesUnicos.slice(-3);
+
+      const preanterior = preanteriorMes ? this.obtenerAcumuladoPorMes(preanteriorMes) : Array(31).fill(0);
+      const anterior = anteriorMes ? this.obtenerAcumuladoPorMes(anteriorMes) : Array(31).fill(0);
+      const actual = actualMes ? this.obtenerAcumuladoPorMes(actualMes) : Array(31).fill(0);
+      
+      this.DataGastosAcumuladosAPI
+      
+      
+      this.DataGastosAcumulados = {
+        labels: labels,
+        datasets: [
+          {
+            label: 'Actual',
+            data: actual,
+            fill: true,
+            backgroundColor: [
+              'rgba(28, 30, 150, 0.37)'
+            ],
+            borderColor: ['rgb(15, 13, 131)'],
+            borderWidth: 1,
+          },
+          {
+            label: 'Anterior',
+            data: anterior,
+            fill: true,
+            backgroundColor: [
+              'rgba(78, 180, 124, 0.36)'
+            ],
+            borderColor: ['rgb(29, 124, 68)'],
+            borderWidth: 1,
+          },
+          {
+            label: 'Preanterior',
+            data: preanterior,
+            fill: true,
+            backgroundColor: [
+              'rgba(196, 226, 25, 0.37)'
+            ],
+            borderColor: ['rgb(205, 219, 4)'],
+            borderWidth: 1,
+          },
+        ],
+      };
+
+      this.OptionsGastosAcumulados = {
+        plugins: {
+          legend: {
+            labels: {
+              color: textColor,
+            },
+          },
+        },
+        scales: {
+          x: {
+            ticks: {
+              color: textColorSecondary,
+            },
+            grid: {
+              color: surfaceBorder,
+            },
+          },
+          y: {
+            beginAtZero: true,
+            ticks: {
+              color: textColorSecondary,
+            },
+            grid: {
+              color: surfaceBorder,
+            },
+          },
+        },
+      };
+      this.cd.markForCheck()
+    }
+  }
+
+  obtenerAcumuladoPorMes(mes: string): number[] {
+    const arr = Array(31).fill(0);
+  
+    // Filtrar datos del mes
+    const movimientosMes = this.DataGastosAcumuladosAPI.filter(d => d.fecha.startsWith(mes));
+  
+    // Sumar día por día
+    for (const mov of movimientosMes) {
+      const dia = parseInt(mov.fecha.slice(8, 10)); // '2025-07-01' → 1
+      arr[dia - 1] += mov.monto; // Sumar monto del día
+    }
+  
+    // Convertir a acumulado progresivo
+    for (let i = 1; i < arr.length; i++) {
+      arr[i] += arr[i - 1];
+    }
+  
+    return arr;
   }
 
 }
